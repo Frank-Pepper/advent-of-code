d53303a5432e77ff6a69de1c9dd84d294ae7023d
Frank <reppepf@gmail.com>
2024-12-01 15:12:24 +0100

    day 1

---
+++ 2024/day1/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func MultipleLines(fileName string) ([]string, error) {
+	inputSlice := make([]string, 0)
+	file, err := os.Open(fileName)
+	if err != nil {
+		return inputSlice, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputSlice = append(inputSlice, scanner.Text())
+	}
+	err = file.Close()
+	if err != nil {
+		return inputSlice, err
+	}
+	return inputSlice, nil
+}
+
+func abs(distance int) int {
+	if distance < 0 {
+		return -distance
+	}
+	return distance
+}
+
+func part1(left []int, right []int, size int) int {
+	var total = 0
+	for i := 0; i < size; i++ {
+		distance := left[i] - right[i]
+		total += abs(distance)
+	}
+	return total
+}
+
+func part2(left []int, right []int, size int) int {
+	var lmap map[int]int
+	lmap = make(map[int]int)
+	var rmap map[int]int
+	rmap = make(map[int]int)
+	for i := 0; i < size; i++ {
+		lmap[left[i]] += 1
+		rmap[right[i]] += 1
+	}
+	var result = 0
+	for i := 0; i < size; i++ {
+		result += rmap[left[i]] * left[i]
+	}
+	return result
+}
+
+func main() {
+
+	lines, err := MultipleLines("input.txt")
+
+	if err != nil {
+		fmt.Print("dupa")
+	}
+
+	fmt.Println(len(lines))
+
+	size := len(lines)
+	left := make([]int, size)
+	right := make([]int, size)
+
+	for index, line := range lines {
+		dupa := strings.Split(line, "   ")
+		left[index], err = strconv.Atoi(dupa[0])
+		right[index], err = strconv.Atoi(dupa[1])
+	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+
+	fmt.Println(part1(left, right, size))
+
+	fmt.Println(part2(left, right, size))
+}
